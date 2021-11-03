8c347df8400e615764635b11161992ac2843b045
NTD1512 <91944903+NTD1512@users.noreply.github.com>
2021-11-03 17:07:59 +0700

    Add files via upload

---
+++ src/Week1/UnionFind.java
@@ -0,0 +1,21 @@
+package Week1;
+
+public class UnionFind {
+    public static void main(String[] args) {
+
+    }
+        static int find( int parent[], int i)
+        {
+            if (parent[i] == -1)
+                return i;
+            return find(parent, parent[i]);
+        }
+
+        // Naive implementation of union()
+        static void Union ( int parent[], int x, int y)
+        {
+            int xset = find(parent, x);
+            int yset = find(parent, y);
+            parent[xset] = yset;
+        }
+}
